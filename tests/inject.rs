1ebe8866f007b854047f56d2d2a46a19a3780cd5
Emmanuel Olowe <e.a.olowe@sms.ed.ac.uk>
2025-06-04 17:16:29 +0100

    fix injection parsing and add tests

---
+++ tests/inject.rs
@@ -0,0 +1,45 @@
+use curly::{Injector, InjectOperation};
+use std::fs;
+use tempfile::tempdir;
+
+#[test]
+fn inject_plain_path() {
+    let dir = tempdir().unwrap();
+    let repo = dir.path();
+    fs::create_dir_all(repo.join("src")).unwrap();
+    fs::write(repo.join("src/lib.rs"), "fn old() {}\n").unwrap();
+
+    let input = repo.join("input.in");
+    fs::write(
+        &input,
+        "src/lib.rs\n```rust\nfn new_fn() {}\n```\n",
+    )
+    .unwrap();
+
+    let injector = Injector::default();
+    injector.inject(&input, repo).unwrap();
+
+    let contents = fs::read_to_string(repo.join("src/lib.rs")).unwrap();
+    assert!(contents.contains("new_fn"));
+}
+
+#[test]
+fn inject_backticked_path() {
+    let dir = tempdir().unwrap();
+    let repo = dir.path();
+    fs::create_dir_all(repo.join("src")).unwrap();
+    fs::write(repo.join("src/lib.rs"), "fn start() {}\n").unwrap();
+
+    let input = repo.join("input.in");
+    fs::write(
+        &input,
+        "### `src/lib.rs`\n```rust\nfn added() {}\n```\n",
+    )
+    .unwrap();
+
+    let injector = Injector::default();
+    injector.inject(&input, repo).unwrap();
+
+    let contents = fs::read_to_string(repo.join("src/lib.rs")).unwrap();
+    assert!(contents.contains("added"));
+}
